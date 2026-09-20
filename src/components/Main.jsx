19ee00f5ea28c1c8088e7b6bbfed251383b866ca
mukunda001 <mukunda8702@gmail.com>
2026-09-20 18:18:34 +0545

    useEffect Demonstration to fetch data from API

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 export default function Main() {
 
@@ -7,6 +7,24 @@ export default function Main() {
         bottomText:"Walk into Mordor",
         imageURL:"http://i.imgflip.com/1bij.jpg"
     })
+
+    const [allMemes, setAllMemes] = useState([])
+    
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+     
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const newMemeUrl = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imageURL: newMemeUrl
+        }))
+    }
 
     function handleChange(event){
         const {value, name} = event.currentTarget
@@ -36,7 +54,7 @@ export default function Main() {
                         onChange={handleChange}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
                 <img src={meme.imageURL} />
