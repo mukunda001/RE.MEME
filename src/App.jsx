47872a782e17202c4d200c32d29ceb9cdee5b1f8
mukunda001 <mukunda8702@gmail.com>
2026-09-23 21:29:47 +0545

    Creating dice components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+import Die from "./components/Die"
+
+
+
 export default function App() {
-    return <main>Type something here</main>
+
+    function generateAllNewDice(){
+    const randomNumbers = [];
+    for (let i = 0; i < 10; i++) {
+    const randomNum = Math.floor(Math.random() * 6) + 1
+    randomNumbers.push(randomNum);
+}
+return randomNumbers;
+}
+console.log(generateAllNewDice());
+
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+            </div>
+        </main>
+    )
 }
